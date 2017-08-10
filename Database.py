bb24d92abac93fc01000a67678415fffa2ba9a50
Jonathan Smith <30507376+Jono408@users.noreply.github.com>
2017-08-10 20:08:28 +0100

    Removed hard-coded paths/names

--- Database.py
+++ Database.py
@@ -1,10 +1,10 @@
 import pyodbc
 
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-F4TU9PM\LOCAL;DATABASE=ItemComparer;TRUSTED_CONNECTION=yes;')
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-.\LOCAL;DATABASE=ItemComparer;TRUSTED_CONNECTION=yes;')
 
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT ShopId, [Name] FROM dbo.Shop WHERE [Name] = 'Tesco'")
+cursor.execute("SELECT ShopId, [Name] FROM dbo.Shop WHERE [Name] = ''")
 row = cursor.fetchone()
 if row:
     print(row)
